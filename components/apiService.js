8879a1be16ecfc797d9ce5cbd7ac2a378b8ec590
Arnold <arnold.chan@lndata.com>
2020-10-08 08:49:06 +0800

    feat: :sparkles: base fetch from db

---
+++ components/apiService.js
@@ -0,0 +1,94 @@
+const BASE_URL = "http://localhost:8001"
+
+export class API {
+    constructor(){
+        const token = "12f17f8d7daa4f40a6155138e9894bfecccb1598"
+        this.token = token;
+    }
+    static loginUser(body){
+        return fetch(`${BASE_URL}/auth/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+            body: JSON.stringify( body )
+        }).then ( resp => resp.json())
+    }
+    static registerUser(body){
+        return fetch(`${BASE_URL}/api/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+                body: JSON.stringify( body )
+            }).then ( resp => resp.json())
+    }
+
+    getMovies(){
+        return fetch(`${BASE_URL}/api/movies/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${this.token}`,
+            }
+            })
+            .then( resp => resp.json())
+            .catch( error => console.log(error))
+    }
+    updateMovie(mov_id, body){
+        return fetch(`${BASE_URL}/api/movies/${mov_id}/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${this.token}`,
+        },
+            body: JSON.stringify( body )
+        })
+    }
+
+    createMovie(body){
+        return fetch(`${BASE_URL}/api/movies/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${this.token}`,
+        },
+            body: JSON.stringify( body )
+        })
+    }
+
+    updateRating(mov_id, rate) {
+        return fetch(`${BASE_URL}/api/movies/${mov_id}/rate_movie/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${this.token}`,
+        },
+            body: JSON.stringify( {stars: rate + 1} )
+        })
+        .then( resp => resp.json())
+        .then( resp => this.getDetails(mov_id))
+        .catch( error => console.log(error))
+        }
+
+    getDetails(mov_id) {
+        return fetch(`${BASE_URL}/api/movies/${mov_id}/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${this.token}`,
+                }})
+                .then( resp => resp.json())
+                .catch( error => console.log(error))
+        }
+
+    deleteMovie(mov_id) {
+        return fetch(`${BASE_URL}/api/movies/${mov_id}/`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${this.token}`,
+                }})
+                .catch( error => console.log(error))
+        }
+}
--- components/list.js
+++ components/list.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { API } from "./apiService";
 
 export default function MovieList() {
+    const [ movies, setMovies ] = useState([{title:'Rambo',id:1}])
+
+    useEffect(()=>{
+        new API().getMovies()
+        .then(resp => setMovies(resp))
+    }, [])
   return (
     <View style={styles.container}>
-      <Text>POL</Text>
+        <FlatList
+            data={movies}
+            renderItem={({item}) => (
+                <Text key={item.id}>{item.title}</Text>
+            )}
+        />
       <StatusBar style="auto" />
     </View>
   );
