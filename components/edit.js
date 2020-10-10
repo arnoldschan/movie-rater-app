b63d392d075ce4850083de43b3d6d040755b019d
arnold samuel Chan <arnold.chan@lndata.com>
2020-10-10 12:12:44 +0800

    feat: :sparkles: form for edit

--- components/edit.js
+++ components/edit.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet} from "react-native";
+import React, {useState} from 'react';
+import { View, Text, TextInput, StyleSheet, Button} from "react-native";
 
 export default function Edit(props) {
     const movie = props.navigation.getParam('movie')
+    const [ title, setTitle ] = useState(movie.title)
+    const [ description, setDescription ] = useState(movie.description)
+
   return (
     <View style={styles.app}>
-      <Text style={styles.text}>Edit {movie.title}</Text>
+      <Text style={styles.text}>Title</Text>
+      <TextInput style={styles.textInput}
+        placeholder="Title"
+        onTextChange={(text)=>setTitle(text)}
+        value={title}/>
+      <Text style={styles.text}>Description</Text>
+      <TextInput style={styles.textInput}
+        placeholder="Description"
+        onTextChange={(text)=>setDescription(text)}
+        value={description}/>
+        <Button onPress={()=>props.navigation.goBack()}
+        title="Save"/>
     </View>
   );
 }
@@ -28,6 +42,13 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     text:{
-        color: 'white'
+        color: 'white',
+        fontSize: 24
+    },
+    textInput:{
+        margin: 10,
+        fontSize: 24,
+        backgroundColor: 'white',
+        padding: 10,
     },
 })
